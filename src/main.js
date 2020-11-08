63a750a1f27c47419a316ccc9e39a60bdd74e952
Andrei Oprisan <andrei_king1@yahoo.com>
2020-11-08 13:06:04 +0200

    Changed mocked server's condition

--- src/main.js
+++ src/main.js
@@ -2,26 +2,27 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
 import Home from "./views/home.vue";
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faPlus,faEdit,faTrash,faListOl } from '@fortawesome/free-solid-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faPlus,
+  faEdit,
+  faTrash,
+  faListOl,
+} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import { makeServer } from "./server";
-import axios from 'axios';
-import VueAxios from 'vue-axios';
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
-Vue.use(VueAxios, axios)
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-library.add(faPlus,faEdit,faTrash,faListOl);
+Vue.use(VueAxios, axios);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+library.add(faPlus, faEdit, faTrash, faListOl);
 /* Starting the fake server */
-if (process.env.NODE_ENV === "development") {
-  makeServer()
-}
+makeServer();
 
-const routes = [
-  { path: "/", component: Home },
-];
+const routes = [{ path: "/", component: Home }];
 
 const router = new VueRouter({
   mode: "history",
